6fc56eed0ac340b41a666230683254b0c99127bd
juliakhlebnikova <66624602+juliakhlebnikova@users.noreply.github.com>
2021-03-23 23:51:37 +0300

    Add files via upload

---
+++ 6ttt/6ttt.cpp
@@ -0,0 +1,41 @@
+﻿#include <vector>
+#include <algorithm>
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+vector<pair<int, int>> F(vector<pair<int, int>> a)
+{
+    vector<pair<float, pair<int, int>>> b;
+    for (auto& i : a)
+    {
+        b.push_back(make_pair(sqrt((i.first) * (i.first) + (i.second) * (i.second)), i));
+    }
+    sort(b.begin(), b.end());
+    vector<pair<int, int>> res;
+    for (auto& i : b)
+    {
+        res.push_back(i.second);
+    }
+    return res;
+}
+
+int main()
+{
+    int n;
+    cout << "Enter n: ";
+    cin >> n;
+    vector<pair<int, int>> a;
+
+    for (int i = 0; i < n; ++i)
+    {
+        int x, y;
+        cout << "Enter x and y: ";
+        cin >> x >> y;
+        a.emplace_back(make_pair(x, y));
+    }
+    vector<pair<int, int>> b = F(a);
+    for (auto& i : b)
+        cout << i.first << " " << i.second << endl;
+}
---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,74 @@
+﻿
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../6ttt/6ttt.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+
+		TEST_METHOD(TestMethod1)
+		{
+			auto ans = F({ { 2, 3 }, {1,2} });
+			vector<pair<int, int>> realAns = { {1, 2}, {2, 3} };
+			Assert::AreEqual(realAns.size(), ans.size());
+			for (int i = 0; i < realAns.size(); ++i)
+			{
+				Assert::AreEqual(realAns[i].first, ans[i].first);
+				Assert::AreEqual(realAns[i].second, ans[i].second);
+			}
+		}
+
+		TEST_METHOD(TestMethod2)
+		{
+			auto ans = F({ { 1, 1 }, {1, 2}, {3, 5} });
+			vector<pair<int, int>> realAns = { {1, 1}, {1, 2}, {3,5} };
+			Assert::AreEqual(realAns.size(), ans.size());
+			for (int i = 0; i < realAns.size(); ++i)
+			{
+				Assert::AreEqual(realAns[i].first, ans[i].first);
+				Assert::AreEqual(realAns[i].second, ans[i].second);
+			}
+		}
+
+		TEST_METHOD(TestMethod3)
+		{
+			auto ans = F({});
+			vector<pair<int, int>> realAns = {};
+			Assert::AreEqual(realAns.size(), ans.size());
+			for (int i = 0; i < realAns.size(); ++i)
+			{
+				Assert::AreEqual(realAns[i].first, ans[i].first);
+				Assert::AreEqual(realAns[i].second, ans[i].second);
+			}
+		}
+
+		TEST_METHOD(TestMethod4)
+		{
+			auto ans = F({ { 1, 0 }, {-1, 0}, {0, 1} });
+			vector<pair<int, int>> realAns = { {-1, 0}, {0, 1}, {1, 0} };
+			Assert::AreEqual(realAns.size(), ans.size());
+			for (int i = 0; i < realAns.size(); ++i)
+			{
+				Assert::AreEqual(realAns[i].first, ans[i].first);
+				Assert::AreEqual(realAns[i].second, ans[i].second);
+			}
+		}
+
+		TEST_METHOD(TestMethod5)
+		{
+			auto ans = F({ { 8, 7 }, {1, 3}, {3, 5} });
+			vector<pair<int, int>> realAns = { {1, 3}, {3, 5}, {8, 7} };
+			Assert::AreEqual(realAns.size(), ans.size());
+			for (int i = 0; i < realAns.size(); ++i)
+			{
+				Assert::AreEqual(realAns[i].first, ans[i].first);
+				Assert::AreEqual(realAns[i].second, ans[i].second);
+			}
+		}
+	};
+}
